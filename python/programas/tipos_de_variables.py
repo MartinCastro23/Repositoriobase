df8b3ff305f5cbb901707ba68413c53ec6e308cd
MartinCastro <martinbornais@gmail.com>
2023-11-29 10:17:30 +0100

    65

---
+++ python/programas/tipos_de_variables.py
@@ -0,0 +1,9 @@
+entero = 33 #asignamos entero al valor 3
+decimal = 0.33#asignamos decimal al valor 0.33
+complejo = 3-3j#asignamos coplejos al valor 3-3j
+texto = "antonio enano"#asignamos texto a "antonio enano"
+
+print ("la variable entero es de type",(entero))
+print ("la variable decimal es de type",(decimal))
+print ("la variable complejo es de type",(complejo))
+print ("la variable texto es de type",(texto))
