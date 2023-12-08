5ea23994e64f2e7d8db26c28d2b6567c13231231
MartinCastro <martinbornais@gmail.com>
2023-12-08 12:20:45 +0100

    67

---
+++ python/programas/variables.py
@@ -0,0 +1,24 @@
+#trabajando en phyton con variables
+a=77 #asignamos a la variable a su valor (77)
+b=33 #asignamos a la variable b su valor (33)
+
+resultado=a+b #asignamos el valor de resultado en caso de suma
+print (("resultado de la suma="),+(resultado)) # hacemos que se imprima la suma
+
+resultado=a-b #asignamos el valor de resultado en caso de resta
+print (("resultado de la resta="),+(resultado)) # hacemos que se imprima la resta
+
+resultado=a*b #asignamos el valor de resultado en caso de multiplicación
+print (("resultado de la multiplicación="),+(resultado)) # hacemos que se imprima la multiplicación
+
+resultado=a/b #asignamos el valor de resultado en caso de división
+print (("resultado de la división="),+(resultado)) # hacemos que se imprima la división
+
+resultado=a%b #asignamos el valor de resultado en caso de módulo
+print (("resultado del módulo="),+(resultado)) # hacemos que se imprima el módulo
+
+resultado=a**b #asignamos el valor de resultado en caso de exponente
+print (("resultado del exponente="),+(resultado)) # hacemos que se imprima el exponente
+
+resultado=a//b #asignamos el valor de resultado en caso de dvisión entera
+print (("resultado de la división entera="),+(resultado)) # hacemos que se imprima la división entera
