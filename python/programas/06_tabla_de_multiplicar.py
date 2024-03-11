a47408d305a0230e7fb0ed3d3224adcf1a5b86c3
MartinCastro <martinbornais@gmail.com>
2024-03-11 10:17:48 +0100

    tabla de multiplicar

---
+++ python/programas/06_tabla_de_multiplicar.py
@@ -0,0 +1,3 @@
+for i in range(1, 10): # Este bucle `for` recorre los números del 1 al 9 y asigna cada número a la variable "i"
+    for j in range(1, 11): # Este bucle `for` recorre los números del 1 al 9 y asigna cada número a la variable "j"
+        print(f"{i} x {j} = {i*j}") #digo que me imprima la multiplicación de "i" por "j"
